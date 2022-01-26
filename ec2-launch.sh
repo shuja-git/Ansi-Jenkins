d2c5312a754c0c1d0a3318487a6b6215f97baf02
shuja <shuja.devops@gmail.com>
2022-01-26 23:09:20 +0530

    run instance

--- ec2-launch.sh
+++ ec2-launch.sh
@@ -3,6 +3,12 @@
 #1. Create a Spot Instance
 #2. Take take instance IP and register in DNS
 
-aws ec2 request-spot-instances --instance-count 1 --type "persistent" --launch-specification file://spot.json --tag-specifications "ResourceType=spot-instances-request,Tags=[{Key=Name,Value=frontend}]" | jq
+#--------------this is to create spot instance request
+#aws ec2 request-spot-instances --instance-count 1 --type "persistent" --launch-specification file://spot.json --tag-specifications "ResourceType=spot-instances-request,Tags=[{Key=Name,Value=frontend}]" | jq
+#-------------------------------------------------------------------------------------------
 
+TEMP_ID="lt-06e98f843b7024a45"
+TEMP_VER=5
+
+aws ec2 run-instances --launch-template LaunchTemplateId=${TEMP_ID},Version=${TEMP_VER} | jq
 
