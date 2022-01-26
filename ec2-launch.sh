b2e6590efe34342bf3c607166db66e17a4efe8a9
shuja <shuja.devops@gmail.com>
2022-01-26 23:35:42 +0530

    running instance from launch templ

--- ec2-launch.sh
+++ ec2-launch.sh
@@ -3,8 +3,15 @@
 #1. Create a Spot Instance
 #2. Take take instance IP and register in DNS
 
-aws ec2 request-spot-instances --instance-count 1 --type "persistent" --launch-specification file://spot.json --tag-specifications "ResourceType=spot-instances-request,Tags=[{Key=Name,Value=frontend}]"
+#aws ec2 request-spot-instances --instance-count 1 --type "persistent" --launch-specification file://spot.json --tag-specifications "ResourceType=spot-instances-request,Tags=[{Key=Name,Value=frontend}]"
 
+
+
+TEMP_ID="lt-06e98f843b7024a45"
+TEMP_VER=5
+
+# check if instance is already there
+aws ec2 run-instances --launch-template LaunchTemplateId=${TEMP_ID},Version=${TEMP_VER}  --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=frontend}]" "ResourceType=spot-instances-request,Tags=[{Key=Name,Value=front}]" | jq
 
 
 #-------------------------------------------------------
