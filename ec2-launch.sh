05fc4cab4d2efb469a330994e2fc68a40f10f570
shuja <shuja.devops@gmail.com>
2022-01-26 23:27:17 +0530

    tags spot instance

--- ec2-launch.sh
+++ ec2-launch.sh
@@ -10,5 +10,5 @@
 TEMP_ID="lt-06e98f843b7024a45"
 TEMP_VER=5
 
-aws ec2 run-instances --launch-template LaunchTemplateId=${TEMP_ID},Version=${TEMP_VER}  --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=frontend}]" "ResourceType=spot-instances-request,Tags=[{Key=Name,Value=frontend}]" | jq
+aws ec2 run-instances --launch-template LaunchTemplateId=${TEMP_ID},Version=${TEMP_VER}  --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=frontend}]" "ResourceType=spot-instances-request,Tags=[{Key=Name,Value=front}]" | jq
 
